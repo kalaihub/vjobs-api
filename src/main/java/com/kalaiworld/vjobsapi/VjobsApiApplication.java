1733c28320f786811a8648860ec933f0cf3818e6
kalai <5819845+kalaihub@users.noreply.github.com>
2020-01-12 18:53:57 -0600

    Spring boot initialization

---
+++ src/main/java/com/kalaiworld/vjobsapi/VjobsApiApplication.java
@@ -0,0 +1,13 @@
+package com.kalaiworld.vjobsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjobsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VjobsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalaiworld/vjobsapi/VjobsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalaiworld.vjobsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VjobsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
